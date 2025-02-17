25aa2e7c779bb03f2dece295452aab1d6b92cfad
Nicolas <nosoyna1des17@gmail.com>
2025-02-17 11:46:55 -0300

    Primer commit

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,34 @@
+import "../hojas-de-estilo/Testimonio.css";
+import PropTypes from "prop-types";
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-tesimonio">
+      <img
+        className="imagen-testimonio"
+        src={`src/imagenes/testimonio-${props.imagen}.png`}
+        alt={`Imagen de ${props.nombre}`}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">&quot;{props.testimonio}&quot;</p>
+      </div>
+    </div>
+  );
+}
+
+Testimonio.propTypes = {
+  imagen: PropTypes.string.isRequired,
+  nombre: PropTypes.string.isRequired,
+  pais: PropTypes.string.isRequired,
+  cargo: PropTypes.string.isRequired,
+  empresa: PropTypes.string.isRequired,
+  testimonio: PropTypes.string.isRequired,
+};
+
+export default Testimonio;
